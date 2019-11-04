0a83df7b32fe9db1d2bc8be41502cb33226acfed
MacUser <maxvandegar@gmail.com>
2019-11-05 00:32:24 +0100

    update to communicate with the CAN

--- CodeCan.py
+++ CodeCan.py
@@ -27,74 +27,199 @@ from numpy import *
 from scipy import interpolate
 from Functions.stdAtmos import *
 
+# Communication with the CAN
+from PCANBasic import *
 
-## Définition des modèles/fonctions
+KALMAN_BOARD_ID = 0
 
-rho=lambda alt:  -9.9814e-05*alt+1.1037; 
+DATA_ID_ACCELERATION_X = 1
+DATA_ID_ACCELERATION_Y = 2
+DATA_ID_ACCELERATION_Z = 3
 
-## Définition des différentes Variables 
 
+def readCanData():
+
+	readResult = objPCAN.Read(PCAN_USBBUS1);
+	while (readResult[1].getID() != KALMAN_BOARD_ID): # We expect results from the KALMAN BOARD
+			readResult = objPCAN.Read(PCAN_USBBUS1)
+	data = readResult[1].DATA >> 4
+	msg_id = (readResult[1].DATA << 4) >> 7
+
+	return data
+
+def SendCanData(data, msg_id):
+	MessageBuffer = TPCANMsg()
+
+	MessageBuffer.ID = 0
+	MessageBuffer.MSGTYPE = PCAN_MESSAGE_STANDARD # can also try PCAN_MESSAGE_EXTENDED
+	MessageBuffer.LEN = 8
+	MessageBuffer.DATA = (data << 4) + (msg_id << 3)
+
+	objPCAN.Write(PCAN_USBBUS1, MessageBuffer);
+
+
+# Initialize the Plug & Play Channel (PCAN-USB)
+objPCAN = PCANBasic()
+result = objPCAN.Initialize(PCAN_USBBUS1, PCAN_BAUD_250K)
+if result != PCAN_ERROR_OK:
+    # An error occurred
+    result = objPCAN.GetErrorText(result)
+    print(result[1])
+else:
+    print("PCAN-USB (Ch-1) was initialized")
+
+
+# Définition des modèles/fonctions + Variables
+
+# On défini les objets Rocket et Environment avec les propriétés provenant
+# de fichier texte
 Rocket = rocketReader('Rocket_Definition_Eiger_I_R03.txt');
-Environnement = environnementReader('Environnement_Definition_USA.txt');
+Environment = environnementReader('Environnement_Definition_USA.txt');
 
 dt=0.25; # Intervalle de temps pour le controle
-x=750;
-v=250;
-[_, a, _, _, nu] = stdAtmos(x,Environnement);
-CD0=drag(Rocket,0,v,nu,a);
-CD_ABferm=drag_shuriken(Rocket,-190.5,0, v, nu);
+
+rho=lambda alt:  -9.9814e-05*alt+1.1037; 
+# Modèle de la densité fonction de l'altitude...
+# ce modèle va dépendre du modèle de l'environnement...
+# on va créer une fonction qui crée cette fonction ...
+# en fonction du fichier texte qui définie l'environnement...
+# un peu plus tard --> pour l'instant utiliser cette fonction de la densité
 g=9.81;
 
-Xvect=[x];
+
+#### TODO ####
+print('TODO: Ask Thomas')
+P0=Environment.Pressure_Ground;
+# L'avionics à besoin d'avoir la pression au sol pour le Kalman, on lui
+# envoie donc P0 avant de commencer la transmission (demander précison à Thomas)
+
+# L'avionics à besoin d'avoir la pression au sol pour le Kalman, on lui
+# envoie donc P0 avant de commencer la transmission (demander précison à Thomas)
+
+# On défini des vecteurs qui vont stocker les valeurs de x et v pour chaque pas de temps dans des
+# vecteurs, ces valeurs sont très importantes, ce sont les données exactes
+# de notre simulation qu'on pourra ensuite comparer aux données que nous
+# renvoie le Kalman. On va jouer avec le bruit des données de sensors pour
+# tester le Kalman, ces performances seront évaluer en se basant sur la
+# différence entre Xvect et Vvect.
+
+
+#ON LANCE UNE PREMIER PAQUET DE DONNEES POUR L'ETAT POST-POUSSEE :
+
+theta=-190.5 # On intie théta avec la valeur de l'angle d'ouverture des airbrakes post poussée,
+# Chaque itération, cette valeur prendra celle donnée par le CAN bus à
+# l'itération précédente qui est calculé par l'algo de contrôle.
+
+x=750; # On initie x avec l'altitude initiale post poussée, chaque itération x sera updater avec la valeur calculé par notre simulation
+v=250; # On initie v avec l'altitude initiale post poussée, chaque itération v sera updater avec la valeur calculé par notre simulation
+
+Xvect=[x];  
 Vvect=[v];
 
 
-Pdyn=0.5*v**2*rho(x);
-Pdynb = Pdyn # +...
-    
-ax=-g-(0.5*rho(x)*(CD0+CD_ABferm)*Rocket.Sm*v**2)/Rocket.rocket_m;
-axb=ax # + ...
-
-axvect=[ax];
-axbvect=[axb];
-Pdynvect=[];
-Pdynbvect=[]; 
-
-# Les valeurs de x et v vont changer à toutes les itérations de la boucle
-
-[_, a, _, _, nu] = stdAtmos(x,Environnement);
+[_, a, Pstat, _, nu] = stdAtmos(x,Environment);
 CD0=drag(Rocket,0,v,nu,a);
 CD_ABferm=drag_shuriken(Rocket,-190.5,0, v, nu);
-Mdry=26.13; 
-A=-9.81;
-B=-0.5*rho(x)*Rocket.Sm*(CD0+CD_ABferm)/Rocket.rocket_m;
 
-f2=lambda t:  sqrt(A/B)*tan(A*sqrt(B/A)*t+atan(v*sqrt(B/A)));
-v=f2(dt);
+#  Attention, ici c'est Pstat et non Pdyn comme dans le code de départ
+Pstatb = Pstat # +... 
+# 'le +...' en commentaire de la ligne précédente signifie qu'on va 
+# rajouter du bruit à Pstat et stocker la nouvelle valeur bruitée dans la
+# variable Pstatb, on va faire tourner plusieurs simulation avec plusieurs
+# valeurs du bruit pour tester le Kalman. Prendre un bruit gaussien à
+# chaque fois et faire varier l'écart type / la moyenne. Plus on a de
+# donnée, mieux on pourra travailler et optimiser le controle des
+# aérofreins
 
-# Calcul de l'altitude (solution exacte) (fct de t et B qui dépend de théta)
-f= lambda t :  (cos(A*sqrt(B/A)*t+atan(v*sqrt(B/A))));
-y=f(dt);
-C=x-log(f(0))/sqrt(A*B)*sqrt(A/B);
-x= C+log(y)/(sqrt(A*B))*sqrt(A/B);
-
-Xvect=[Xvect, x];
-Vvect=[Vvect, v];
+ax=-g-(0.5*rho(x)*(CD0+CD_ABferm)*Rocket.Sm*v**2)/Rocket.rocket_m;
+axb=ax # + ...
+# Pareillement, on rajoute du bruit sur l'accélération de la fusée.
 
 
-Pdyn=0.5*v**2*rho(x);
-Pdynb=Pdyn # +...;
-Pdynvect=[Pdynvect, Pdyn];
-Pdynbvect=[Pdynbvect, Pdynb];
+# On stocke les valeures provenant des sensors
+axvect=[ax];
+axbvect=[axb];
+Pstatvect=[Pstat];
+Pstatbvect=[Pstatb]; 
+
+# CALCULE DE LA TRAJECTOIRE  : ON CALCULE TOUT EN SUPPOSANT 
+# UNE OUVERTURE INSTANTANNEE DES AIRBRAKES, ET UNE CHAINE AVIONICS
+# IMMEDIATE POUR L'INSTANT
+
+
+while(v>0.01):
+
+	Sleep(1000);
+
+	# send data to the CAN
+	print("Q: --> imub[0]--> 0 à 5?")
+	SendCanData(0., DATA_ID_ACCELERATION_X) # acc x
+	SendCanData(0., DATA_ID_ACCELERATION_Y) # acc y
+	SendCanData(x, DATA_ID_ACCELERATION_Z) # acc z
+	SendCanData(0., TODO) # angle rate
+	SendCanData(0., TODO) # angle rate
+	SendCanData(0., TODO) # angle rate
+	SendCanData(Pstatvect[-1], TODO) # pression statique (à cette altitude)
+	SendCanData(P0, TODO) # pression de base (à l’altitude delancement)
+	SendCanData(0., TODO) # Zdata 0
+	SendCanData(0., TODO) # Zdata 1
+	SendCanData(0., TODO) # Zdata 2
+	print("Q: What to send here????")
+	SendCanData(0., TODO) # Zdata.3= Alt(P) altitude en fonction de la pression
+
+	# read theta from the CAN
+	theta = readCanData()
+
+	# On update le modèle du drag selon la valeur de théta
+	CD0=drag(Rocket,0,v,nu,a); # Drag de la fusée à angle d'attaque 0 (1D pour l'instant)
+	CD_ABferm=drag_shuriken(Rocket,theta,0, v, nu); # Drag des aérofreins à angle d'attaque 0 (1D pour l'instant)
+
+	#  B est une constante qui dépend de l'altitude qu'on utilise pour la
+	#  résolution physique des équations
+	A=-9.81;
+	B=-0.5*rho(x)*Rocket.Sm*(CD0+CD_ABferm)/Rocket.rocket_m;
+	#  Rocket.rocket_m = masse de la fusée post poussée
+	#  Rocket.Sm = surface frontale de la fusée 
+
+	# On crée une fonction f2 qui nous permets de trouver la vitesse à un temps
+	# t après les conditions initiale
+	f2=lambda t:  sqrt(A/B)*tan(A*sqrt(B/A)*t+atan(v*sqrt(B/A)));
+	v=f2(dt);
+	Vvect.append(v);
+	# Calcul de l'altitude (solution exacte) (fct de t et B qui dépend de théta)
+	# Pour faire ca on procède en plusieurs étapes (trois lignes suivantes)
+	f= lambda t :  (cos(A*sqrt(B/A)*t+atan(v*sqrt(B/A))));
+	y=f(dt);
+	C=x-log(f(0))/sqrt(A*B)*sqrt(A/B);
+	x= C+log(y)/(sqrt(A*B))*sqrt(A/B);
+	Xvect.append(x);
+
+	## On recalcule les données à envoyer au CAN pour le nouvel état (état=x,v)
+	[_, _, Pstat, _, nu] = stdAtmos(x, Environment);
+	Pstatb=Pstat # +...; # bruit sera le meme tout le temps, on prendra une Gaussienne
+
+	Pstatvect.append(Pstat);
+	Pstatbvect.append(Pstatb);
+	ax=(Vvect[-1]-Vvect[-2])/dt;
+	axb=ax  # +...,
+	axvect.append(ax);
+	axbvect.append(axb);
+
+	print(x)
+	print(v)
+
+
+
+# Release the USB Channel
+result = objPCAN.Uninitialize(PCAN_USBBUS1)
+if result != PCAN_ERROR_OK:
+    # An error occurred, get a text describing the error and show it
     
-ax=(Vvect[-1]-Vvect[-2])/dt;
-axb=ax # +...,
-axvect=[axvect, ax];
-axbvect=[axbvect, axb];
+    result = objPCAN.GetErrorText(result)
+    print(result[1])
+else:
+    print("PCAN-USB (Ch-1) was released")
 
-print(axvect)
-print(Xvect)
-print(Vvect)
-print(Pdynvect)
-print(Pdynbvect)
+
+
 
